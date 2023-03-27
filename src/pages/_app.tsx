8894898023ddf5a848f2285d9c89f72b4562e1c4
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-03-27 17:13:35 +0700

    add date picker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,23 @@ import Layout from "../components/layout";
 import { useRouter } from "next/router";
 import { SolarHeader, WindHeader } from "@/components/headers";
 
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <>
-      <Layout>
-        <>
-          {router.pathname.startsWith("/panel-surya") && <SolarHeader />}
-          {router.pathname.startsWith("/turbin-angin") && <WindHeader />}
-          <Component {...pageProps} />
-        </>
-      </Layout>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Layout>
+          <>
+            {router.pathname.startsWith("/panel-surya") && <SolarHeader />}
+            {router.pathname.startsWith("/turbin-angin") && <WindHeader />}
+            <Component {...pageProps} />
+          </>
+        </Layout>
+      </LocalizationProvider>
     </>
   );
 }

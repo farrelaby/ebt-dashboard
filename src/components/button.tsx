a0e8972078e65471f1360e8ee06fc22532f7baca
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-03-27 17:15:53 +0700

    create download button component

---
+++ src/components/button.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@mui/material";
+import Image from "next/image";
+
+type DownloadButtonProps = {
+  onClick: (event: React.MouseEvent) => void;
+};
+
+function DownloadButton({ onClick }: DownloadButtonProps) {
+  return (
+    <Button
+      onClick={onClick}
+      variant="contained"
+      color="primary"
+      size="medium"
+      style={{
+        textTransform: "none",
+        backgroundColor: "#000000",
+      }}
+    >
+      <div className="flex flex-row gap-2">
+        <Image src="/download-logo.svg" alt="" width={20} height={20} />
+        Download Data Mentah
+      </div>
+    </Button>
+  );
+}
+
+export { DownloadButton };

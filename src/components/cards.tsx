ff92429fb6f4569b7dcc5808a9cbf23b768aded6
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-05-12 09:03:18 +0700

    add cards component and types

---
+++ src/components/cards.tsx
@@ -0,0 +1,19 @@
+function RealTimeCard({
+  value,
+  unit,
+  title,
+}: {
+  value: number | undefined | null;
+  unit: string;
+  title: string;
+}) {
+  return (
+    <div className="h-72 w-52 bg-[#f2e9ff] flex flex-col place-items-center justify-center rounded-lg">
+      <h4 className="text-6xl font-semibold">{value ?? "-"}</h4>
+      <p className="text-xl mt-3 text-[#A4A6B3]">{unit}</p>
+      <div className="text-2xl font-semibold mt-9">{title}</div>
+    </div>
+  );
+}
+
+export { RealTimeCard };
---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+export interface RealData {
+  voltage: number;
+  current: number;
+  power: number;
+  energy: number;
+  power_factor: number;
+  db_created_at: string;
+}
